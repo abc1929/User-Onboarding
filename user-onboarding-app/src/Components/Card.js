bd1ef102364fa0975f5def40ff1be4d1639f721a
Jiawei Wu <abc1929@gmail.com>
2021-01-28 07:52:05 +0800

    minor css update

--- user-onboarding-app/src/Components/Card.js
+++ user-onboarding-app/src/Components/Card.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Stack, Box, Text } from "@chakra-ui/react";
+import { Stack, Box, Text, Tooltip } from "@chakra-ui/react";
 
 export default function Card(props) {
-   const { name } = props.userdata;
+   const { name, email } = props.userdata;
 
    return (
       <Stack>
-         <Box
-            // border="1px gray"
-            padding="2%"
-            width="50%"
-            margin="1% 0%"
-            borderRadius="8px"
-            bgColor="#f2f7f7"
-         >
-            <Text> {name} </Text>
-         </Box>
+         <Tooltip label={email} aria-label="A tooltip">
+            <Box
+               className="usercard"
+               // border="1px gray"
+               padding="2%"
+               width="50%"
+               margin="1% 0%"
+               borderRadius="8px"
+               bgColor="#f2f7f7"
+            >
+               <Text> {name} </Text>
+            </Box>
+         </Tooltip>
       </Stack>
    );
 }
